5ffc4daeae9da6dda0c46937e1399758c19a0b09
Kevin Simorangkir <104813623+kevinsimorangkir21@users.noreply.github.com>
2024-09-02 18:27:20 +0700

    push captcha

--- app/components/homepage/contact/contact-without-captcha.jsx
+++ app/components/homepage/contact/contact-without-captcha.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import { useState } from 'react';
 import { TbMailForward } from "react-icons/tb";
 import { toast } from 'react-toastify';
+import { IoSend } from "react-icons/io5";
 
 function ContactWithoutCaptcha() {
   const [error, setError] = useState({ email: false, required: false });
@@ -59,16 +60,17 @@ function ContactWithoutCaptcha() {
       </p>
       <div className="max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5">
         <p className="text-sm text-[#d3d8e8]">
-          {"If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."}
+          {"If you have any questions or concerns, please feel free to contact me. I am open to any job opportunities that match my skills and interests."}
         </p>
         <div className="mt-6 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
-            <label className="text-base">Your Name: </label>
+            <label className="text-base">Your Name</label>
             <input
               className="bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2"
               type="text"
               maxLength="100"
               required={true}
+              placeholder="Enter your name"
               onChange={(e) => setUserInput({ ...userInput, name: e.target.value })}
               onBlur={checkRequired}
               value={userInput.name}
@@ -76,12 +78,13 @@ function ContactWithoutCaptcha() {
           </div>
 
           <div className="flex flex-col gap-2">
-            <label className="text-base">Your Email: </label>
+            <label className="text-base">Your Email </label>
             <input
               className="bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2"
               type="email"
               maxLength="100"
               required={true}
+              placeholder="Enter your email"
               value={userInput.email}
               onChange={(e) => setUserInput({ ...userInput, email: e.target.value })}
               onBlur={() => {
@@ -95,12 +98,13 @@ function ContactWithoutCaptcha() {
           </div>
 
           <div className="flex flex-col gap-2">
-            <label className="text-base">Your Message: </label>
+            <label className="text-base">Your Message</label>
             <textarea
               className="bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2"
               maxLength="500"
               name="message"
               required={true}
+              placeholder="Enter your message"
               onChange={(e) => setUserInput({ ...userInput, message: e.target.value })}
               onBlur={checkRequired}
               rows="4"
@@ -119,7 +123,7 @@ function ContactWithoutCaptcha() {
               onClick={handleSendMail}
             >
               <span>Send Message</span>
-              <TbMailForward className="mt-1" size={18} />
+              <IoSend className="mt-1" size={18} />
             </button>
           </div>
         </div>
--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -5,6 +5,7 @@ import { BiLogoLinkedin } from "react-icons/bi";
 import { CiLocationOn } from "react-icons/ci";
 import { FaFacebook, FaStackOverflow } from 'react-icons/fa';
 import { FaXTwitter } from "react-icons/fa6";
+import { IoMail } from "react-icons/io5";
 import { IoLogoGithub, IoMdCall } from "react-icons/io";
 import { MdAlternateEmail } from "react-icons/md";
 import ContactWithCaptcha from './contact-with-captcha';
@@ -28,7 +29,7 @@ function ContactSection() {
         <div className="lg:w-3/4 ">
           <div className="flex flex-col gap-5 lg:gap-9">
             <p className="text-sm md:text-xl flex items-center gap-3">
-              <MdAlternateEmail
+              <IoMail
                 className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
                 size={36}
               />
