f0ed77f3fcd2edb6cd19128ba6a7ecbd05d1b506
Kevin Simorangkir <104813623+kevinsimorangkir21@users.noreply.github.com>
2025-01-11 15:03:31 +0700

    push baru

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  integrations: [tailwind(), robotsTxt()]
 })
