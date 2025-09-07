ae1591812cca0f42ebf97f39f021e8d00b5caa16
Kevin Simorangkir <104813623+kevinsimorangkir21@users.noreply.github.com>
2025-09-07 17:44:51 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import maintenance from "astro-maintenance";
 import netlify from "@astrojs/netlify/functions"; 
 
 const start = new Date("2025-09-05T19:00:00+07:00").getTime();
-const end   = new Date("2025-09-07T20:30:00+07:00").getTime();
+const end   = new Date("2025-09-07T21:00:00+07:00").getTime();
 
 const now = new Date(
   new Date().toLocaleString("en-US", { timeZone: "Asia/Jakarta" })
@@ -19,8 +19,9 @@ export default defineConfig({
     robotsTxt(),
     maintenance({
       enabled: now >= start && now <= end,
-      template: "/maintenance",
-      allow: ["127.0.0.1"],
+      template: "/maintenance",   // halaman tujuan
+      allow: ["127.0.0.1"],       // IP yang tetap bisa akses
+      redirect: true,             // 🚀 paksa redirect ke /maintenance
     }),
   ],
 });
